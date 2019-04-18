5049c06681afe878aebc77a027d34aac7373a921
Antoine <antoiner@theodo.fr>
2019-04-18 20:38:27 +0200

    remove useless elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import style from './App.module.scss'
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p className={style.text}>
             Edit <code>src/App.js</code> and save to reload.
           </p>
