83fd150a275bf150193c2cb07d776395ac2832ad
Antoine <antoiner@theodo.fr>
2019-04-22 12:58:21 +0200

    add API request

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-// import classname from {classname} classnames(class1, class2)
-
 import style from './Header.module.scss'
+
+var classNames = require('classnames');
+
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <div className={style.header}>
-          <div className={style.header_div}>
-            <img src={require("../img/logo-theodo.png")} alt={'Theodo'} className={style.logo}/>
-            <span className={style.header_text}>Menu Item 1</span>
-            <span className={style.header_text}>Menu Item 2</span>
-            <span className={style.header_text}>Menu Item 3</span>
-          </div>
-          <div className={style.header_div}>
-            <button className={style.header_text}>Subscribe</button>
-            <a className={style.header_text}>Login</a>
+        <div className={style.header_container}>
+          <div className={style.header}>
+            <div className={style.header_div}>
+              <img src={require("../img/logo-cmc.png")} alt={'Theodo'} className={style.logo}/>
+              <span className={style.header_text}>Menu Item 1</span>
+              <span className={style.header_text}>Menu Item 2</span>
+              <span className={style.header_text}>Menu Item 3</span>
+            </div>
+            <div className={style.header_div}>
+              <input className={style.header_search_input} placeholder={'Rechercher'}/>
+              <a className={style.header_text}>Subscribe</a>
+              <button className={classNames(style.header_button)}>Login</button>
+            </div>
           </div>
         </div>
       </div>
-    );
+
+  );
   }
 }
 
--- src/PokemonCard/Card.js
+++ src/PokemonCard/Card.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import style from './Card.module.scss'
 
 class Card extends Component {
@@ -13,14 +12,19 @@ class Card extends Component {
   }
 
   componentDidMount() {
+
     Promise.all([
-      fetch('https://pokeapi.co/api/v2/pokemon/ditto'),
-      fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+      fetch('https://pokeapi.co/api/v2/pokemon/43'),
+      fetch('https://pokeapi.co/api/v2/pokemon/32'),
+      fetch('https://pokeapi.co/api/v2/pokemon/145'),
+      fetch('https://pokeapi.co/api/v2/pokemon/89'),
+      fetch('https://pokeapi.co/api/v2/pokemon/65'),
+      fetch('https://pokeapi.co/api/v2/pokemon/123')
     ])
-        .then(([pokemon1, pokemon2]) => Promise.all([pokemon1.json(), pokemon2.json()]))
-        .then(([pokemonData1, pokemonData2]) => {
+        .then(([pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]) => Promise.all([pokemon1.json(), pokemon2.json(), pokemon3.json(), pokemon4.json(), pokemon5.json(), pokemon6.json()]))
+        .then(([pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]) => {
           this.setState({
-            pokemonList: [pokemonData1, pokemonData2]
+            pokemonList: [pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]
           })
         });
   }
@@ -29,13 +33,22 @@ class Card extends Component {
     const pokemonList = this.state.pokemonList;
     return (
       <div className="Card">
+        <div className={style.content}></div>
+
         {pokemonList.length > 0 &&
-          pokemonList.map(pokemon =>
-              <div className={style.card}>
-                <div>
-                  {pokemon.weight}
-                  {pokemon.sprites.front_default}
-                </div>
+          pokemonList.map((pokemon, index) =>
+              <div>
+                {index+1} &nbsp;
+
+                {pokemon.name} &nbsp;
+
+                {pokemon.sprites.front_default} &nbsp;
+
+                [{pokemon.types.map(typeArray =>
+                    <span> {typeArray.type.name},&nbsp;</span>
+                )}]&nbsp;
+
+                {pokemon.id} &nbsp;
               </div>
           )
         }
