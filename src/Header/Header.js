dcb199e443de146dce89d2f6d0aae853fdcf1c8a
Antoine <antoiner@theodo.fr>
2019-04-30 09:04:16 +0200

    hints

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,10 +9,21 @@ class Header extends Component {
     return (
       <div className="Header">
         <div className={style.header_container}>
+          <div className={style.header_div}>
+            <img src={require("../img/logo-cmp.png")} alt={'Theodo'} className={style.logo}/>
+            <span className={style.header_text}>Menu Item 1</span>
+            <span className={style.header_text}>Menu Item 2</span>
+            <span className={style.header_text}>Menu Item 3</span>
+          </div>
+          <div className={style.header_div}>
+            <input className={style.header_search_input} placeholder={'Rechercher'}/>
+            <a className={style.header_text}>Subscribe</a>
+            <button className={classNames(style.header_button)}>Login</button>
+          </div>
         </div>
       </div>
 
-  );
+    );
   }
 }
 
--- src/PokemonCard/Card.js
+++ src/PokemonCard/Card.js
@@ -21,30 +21,42 @@ class Card extends Component {
       fetch('https://pokeapi.co/api/v2/pokemon/65'),
       fetch('https://pokeapi.co/api/v2/pokemon/123')
     ])
-        .then(([pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]) => Promise.all([pokemon1.json(), pokemon2.json(), pokemon3.json(), pokemon4.json(), pokemon5.json(), pokemon6.json()]))
-        .then(([pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]) => {
-          this.setState({
-            pokemonList: [pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]
-          })
-        });
+      .then(([pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6]) => Promise.all([pokemon1.json(), pokemon2.json(), pokemon3.json(), pokemon4.json(), pokemon5.json(), pokemon6.json()]))
+      .then(([pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]) => {
+        this.setState({
+          pokemonList: [pokemonData1, pokemonData2, pokemonData3, pokemonData4, pokemonData5, pokemonData6]
+        })
+      });
   }
 
   render() {
     const pokemonList = this.state.pokemonList;
     return (
       <div className="Card">
+        <div className={style.content}></div>
+
         {pokemonList.length > 0 &&
-          pokemonList.map((pokemon, index) =>
-              <div>
-                  {index+1} &nbsp;
-                  <img src={pokemon.sprites.front_default} alt="" /> &nbsp;
-                  {pokemon.name} &nbsp;
+        pokemonList.map((pokemon, index) =>
+          <div className={style.card}>
+            <div className={style.cardNumber}>
+              {index+1}
+            </div>
+            <div className={style.cardLogo}>
+              <img src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className={style.cardNameAndTypes}>
+              <span className={style.cardName}>{pokemon.name}</span>
+              <span className={style.cardTypes}>
                     {pokemon.types.map(typeArray =>
-                        <span className={style.cardType}> {typeArray.type.name},&nbsp;</span>
+                      <span className={style.cardType}> {typeArray.type.name},&nbsp;</span>
                     )}
-                  {pokemon.id} &nbsp;
-              </div>
-          )
+                  </span>
+            </div>
+            <div className={style.cardId}>
+              {pokemon.id}
+            </div>
+          </div>
+        )
         }
       </div>
     );
